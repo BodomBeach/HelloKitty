4fe3d849748ec786b8ec1bcee7a7761950890ee8
Mathilde <raymathild@gmail.com>
2018-08-30 12:53:41 +0200

    ajout user controller + view profile + changement nombre items dans un panier + css change settings

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :items
   resources :charges
   devise_for :users
+  get 'user/profile', to: 'user#profile', as: :profile
   get 'carts/show', to: 'carts#show', as: 'cart_show'
   post 'carts/add_item', to: 'carts#add_item', as: 'add_item'
   post 'carts/remove_item', to: 'carts#remove_item', as: 'remove_item'
